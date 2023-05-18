f6cdc366459697dfb7578e402a70be897b2deef7
m-mraisi <mahmoudmraisi2@gmail.com>
2023-05-18 19:18:48 -0400

    Initial Commit

---
+++ hauler/haulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  haulerApp.swift
+//  hauler
+//
+//  Created by Mahmoud Mraisi on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct haulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
