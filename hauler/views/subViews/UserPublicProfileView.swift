93a52c52493704eb1de6a53171b50ea570d8b78c
aratrika <aratrika.mukherjee26@gmail.com>
2023-06-08 18:25:12 -0400

    chnges

---
+++ hauler/views/subViews/UserPublicProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  UserPublicProfileView.swift
+//  hauler
+//
+//  Created by Aratrika Mukherjee on 2023-06-08.
+//
+
+import SwiftUI
+
+struct UserPublicProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserPublicProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserPublicProfileView()
+    }
+}
