8e10a34457488e177886a2dc8f04b3e186304db6
homing1923 <homing1923@gmail.com>
2023-06-14 01:15:40 -0400

    post page ui update

---
+++ hauler/views/subViews/InputField/MaterialDesignTextField.swift
@@ -0,0 +1,20 @@
+//
+//  MaterialDesignTextField.swift
+//  hauler
+//
+//  Created by Homing Lau on 2023-06-13.
+//
+
+import SwiftUI
+
+struct MaterialDesignTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MaterialDesignTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        MaterialDesignTextField()
+    }
+}
