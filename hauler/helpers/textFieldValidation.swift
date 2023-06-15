5257cde3ed9d52e6679c12c095b89f07175cc8c6
homing1923 <homing1923@gmail.com>
2023-06-15 17:02:24 -0400

    validation works for posting view

---
+++ hauler/helpers/textFieldValidation.swift
@@ -0,0 +1,8 @@
+//
+//  textFieldValidation.swift
+//  hauler
+//
+//  Created by Homing Lau on 2023-06-15.
+//
+
+import Foundation
---
+++ hauler/views/subViews/ImagePickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePickerView.swift
+//  hauler
+//
+//  Created by Homing Lau on 2023-06-15.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerView()
+    }
+}
