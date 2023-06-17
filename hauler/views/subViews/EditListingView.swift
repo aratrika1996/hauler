23a0996a92458f726ff0f55126ecbf39c6772c02
m-mraisi <mahmoudmraisi2@gmail.com>
2023-06-17 18:07:07 -0400

    added edit listing view

---
+++ hauler/views/subViews/EditListingView.swift
@@ -0,0 +1,20 @@
+//
+//  EditListingView.swift
+//  hauler
+//
+//  Created by Mahmoud Mraisi on 17/06/2023.
+//
+
+import SwiftUI
+
+struct EditListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditListingView()
+    }
+}
