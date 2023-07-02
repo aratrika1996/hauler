fb44560b6cea609bf84a9595033fcc55316c2474
aratrika <aratrika.mukherjee26@gmail.com>
2023-07-01 21:28:38 -0400

    Added forgot password feature

--- hauler/views/subViews/ForgotPasswordView.swift
+++ hauler/views/subViews/ForgotPasswordView.swift
@@ -8,13 +8,65 @@
 import SwiftUI
 
 struct ForgotPasswordView: View {
+    @EnvironmentObject var authController : AuthController
+    @State private var email : String = ""
+    @State private var isSheetPresent = false
+    @Binding var rootScreen :RootView
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                Section {
+                    TextField("Enter Email", text: self.$email)
+                        .disableAutocorrection(true)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Button(action: {
+                    resetPassword()
+                }){
+                    Text("Send email")
+                        .foregroundColor(Color.red)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+            }
+        }
+        .sheet(isPresented: self.$isSheetPresent, onDismiss: goToLogin) {
+            VStack {
+                Image(systemName: "envelope.badge")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70)
+                    .padding(.bottom, 30)
+                
+                Text("Almost there!")
+                    .padding(.bottom, 10)
+                    .font(.system(size: 30))
+                    .fontWeight(.medium)
+                    .foregroundColor(Color("HaulerOrange"))
+                
+                Text("We have sent a temporary link to your email with further instructions. This link will expire soon.")
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+        }
+        .navigationBarTitle("Reset Password")
+    }
+    
+    func resetPassword() {
+        self.authController.sendPasswordReset(withEmail: self.email) {_ in
+            self.isSheetPresent = true
+        }
+    }
+    
+    func goToLogin(){
+        self.dismiss()
     }
 }
 
-struct ForgotPasswordView_Previews: PreviewProvider {
-    static var previews: some View {
-        ForgotPasswordView()
-    }
-}
+//struct ForgotPasswordView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ForgotPasswordView()
+//    }
+//}
