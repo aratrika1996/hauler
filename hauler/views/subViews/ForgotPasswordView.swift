d57b9d39ef7a463cf66ba1f878976ede64c2a7ac
aratrika <aratrika.mukherjee26@gmail.com>
2023-07-01 21:28:25 -0400

    added forgot password feature

---
+++ hauler/views/subViews/ForgotPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  ForgotPasswordView.swift
+//  hauler
+//
+//  Created by Aratrika Mukherjee on 2023-06-30.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForgotPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForgotPasswordView()
+    }
+}
