c4d49ac415ded17e49e4f81814cb27849a72d8ab
homing1923 <homing1923@gmail.com>
2023-07-18 10:49:16 -0400

    added view

---
+++ hauler/views/subViews/FollowedUserView.swift
@@ -0,0 +1,20 @@
+//
+//  FollowedUserView.swift
+//  hauler
+//
+//  Created by Homing Lau on 2023-07-18.
+//
+
+import SwiftUI
+
+struct FollowedUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FollowedUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowedUserView()
+    }
+}
---
+++ hauler/views/subViews/NotoficationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotoficationView.swift
+//  hauler
+//
+//  Created by Homing Lau on 2023-07-18.
+//
+
+import SwiftUI
+
+struct NotoficationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotoficationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotoficationView()
+    }
+}
